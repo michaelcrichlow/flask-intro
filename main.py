d05f5fc3eab4047915aeaa0a6d54f7f8c8ec7b25
michaelcrichlow <mikecrichlow@gmail.com>
2023-08-06 12:42:12 -0700

    first commit

---
+++ main.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from config import DevConfig
+# from waitress import serve
+
+app = Flask(__name__)
+app.config.from_object(DevConfig)
+
+
+# -----ROUTES------------------------------------------------------------------
+@app.route("/")
+@app.route("/index")
+def index():
+    return "<h1>Hello World! This is some more text.</h1>"
+# -----------------------------------------------------------------------------
+
+
+if __name__ == '__main__':
+    app.run()
+    # serve(app, host="0.0.0.0", port=5000)
